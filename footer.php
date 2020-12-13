b4eb6d12c4c749cd68d259269fd6655f2ed204d6
t-tamaru <t-tamaru@sakura.ad.jp>
2020-12-13 12:48:35 +0900

    footer作成中

--- footer.php
+++ footer.php
@@ -13,6 +13,63 @@
 
 </div><!-- #page -->
 
+<footer class="footer">
+  <div class="footer__toTop"><a href=""><?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/images/icons/to_top.svg'); ?></a></div>
+  <div class="footer__upper">
+    <div class="footer__logo"><a href=""><?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/images/icons/logo_light.svg'); ?></a></div>
+    <nav>
+      <div class="footer__upperLinkBlock">
+        <ul class="footer__upperLinkList">
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">Mamoriとは</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">サービス・設備</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">料金・ご利用の流れ</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">よくあるご質問</a>
+          </li>
+        </ul>
+        <ul class="footer__upperLinkList">
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">お知らせ</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">アクセス</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">お問い合わせ</a>
+          </li>
+          <li class="footer__upperLinkItem">
+            <a href="<?php echo home_url(); ?>">見学予約</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <div class="footer__lower">
+    <nav>
+      <ul class="footer__lowerLinkList">
+        <li class="footer__lowerLinkItem">
+          <a href="<?php echo home_url(); ?>">運営会社</a>
+        </li>
+        <li class="footer__lowerLinkItem">
+          <a href="<?php echo home_url(); ?>">利用規約</a>
+        </li>
+        <li class="footer__lowerLinkItem">
+          <a href="<?php echo home_url(); ?>">個人情報保護方針</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="footer__copyrightWrapper">
+    <small class="footer__copyright">Copyright©2020 株式会社ナマケモノAll Rights Reserved.</small>
+  </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
