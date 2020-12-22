f9404ebd66a95b19b9e8b8f4d09860d4f907f8bb
t-tamaru <t-tamaru@sakura.ad.jp>
2020-12-22 13:52:24 +0900

    一旦push

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,15 +1,32 @@
 "use strict";
 
 jQuery(function(){
-	var scrollPosition;
+	let scrollPosition;
 	jQuery(".side-conversion-link.-primary").on("click", function() {
 		scrollPosition = jQuery(window).scrollTop();
     jQuery('body').addClass('fixed').css({'top': -scrollPosition});
     jQuery('.join-modal').fadeIn(200)
-	});
+
+    if(jQuery('#modal-check').prop('checked')) {
+      jQuery('.join-submit').prop('disabled', false)
+    } else {
+      jQuery('.join-submit').prop('disabled', true)
+    }
+  });
+
 	jQuery(".modal-close-button").on("click", function() {
 		jQuery('body').removeClass('fixed').css({'top': 0});
     window.scrollTo( 0 , scrollPosition );
     jQuery('.join-modal').fadeOut(200)
-	});
+    jQuery('#modal-check').prop('checked', false)
+  });
+
+
+  jQuery('#modal-check').change(function() {
+    if(jQuery('#modal-check').prop('checked')) {
+      jQuery('.join-submit').prop('disabled', false)
+    } else {
+      jQuery('.join-submit').prop('disabled', true)
+    }
+  })
 });
--- template-parts/join-modal.php
+++ template-parts/join-modal.php
@@ -9,10 +9,11 @@
         会員登録をご希望の方は、Mamoriの個人情報保護方針をご一読いただき、<br>
         同意のうえ、下記ボタンより入会フォームにお進みください。
       </p>
-      <label class="modal-input">
-        <input type="checkbox" name="agreedCheckbox" onclick=""><span class="join-agreementFormLabel"><a href="<?php echo home_url('/privacy/') ?>">個人情報保護方針</a>に同意する</span>
-      </label>
-      <button type="submit" onclick="return checkAgree();" class="join-submit">
+      <div class="modal-input">
+        <input type="checkbox" name="agreedCheckbox" id="modal-check">
+        <label for="modal-check" class="join-form-label"><a href="<?php echo home_url('/privacy/') ?>" class="modal-link">個人情報保護方針</a>に同意する</label>
+      </div>
+      <button type="button" class="join-submit" onclick="window.open('//www.kaihipay.jp/forms?form_code=2528567304314701')" disabled>
         <span class="join-submit-inner">入会する</span>
         <div class="join-submit-icon"><?php echo file_get_contents(get_template_directory_uri() . '/assets/images/icons/icon_external.svg'); ?></div>
       </button>
